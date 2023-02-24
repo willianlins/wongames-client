69ce315f6429cc8d18796c302f84e57f10d0ea71
Willian lins araujo <williannlins@gmail.com>
2023-02-24 16:43:23 -0300

    ok

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,33 +1,10 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main`
-  background-color: #06092b;
-  color: #fff;
-  width: 100%;
-  height: 100%;
-  padding: 3rem;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-`
+import { LogoProps } from '.'
 
-export const Logo = styled.img`
-  width: 25rem;
-  margin-bottom: 2rem;
-`
 
-export const Title = styled.h1`
-  font-size: 2.5rem;
-`
-
-export const Description = styled.h2`
-  font-size: 2rem;
-  font-weight: 400;
-`
-
-export const Ilustration = styled.img`
-  margin-top: 3rem;
-  width: min(30rem, 100%);
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, color }) => css`
+    color: ${theme.colors[color!]};
+  `}
 `
