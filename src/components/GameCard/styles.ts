1f8eb087781f2019997c70f310ba7f31b23181f9
Willian lins araujo <williannlins@gmail.com>
2023-05-02 15:52:40 -0300

    ok

--- src/components/GameCard/styles.ts
+++ src/components/GameCard/styles.ts
@@ -2,9 +2,20 @@ import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 
 
-export const Wrapper = styled.article``
+export const Wrapper = styled.article`
+  ${({theme}) => css`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    background-color: ${theme.colors.white};
+  `}
+`
 
 export const ImageBox = styled.div``
+
+export const Content = styled.div``
 
 export const Info = styled.div``
 
@@ -17,3 +28,4 @@ export const FavButton = styled.div``
 export const BuyBox = styled.div``
 
 export const Price = styled.div``
+
